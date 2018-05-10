33a15cb46f7bb61e94214997c5e3beb4e9501594
Isaac Tretta <32001785+IsaacZero@users.noreply.github.com>
2018-05-10 12:58:27 -0600

    Pila lista

--- ListaIndEn.h
+++ ListaIndEn.h
@@ -14,7 +14,63 @@
 #ifndef LISTAINDEN_H
 #define LISTAINDEN_H
 
-
+template < typename V >
+class ListaIndEn{
+public:    
+    
+    ListaIndEn();
+    virtual ~ListaIndEn();
+    
+    //EFE: Inicializa la Lista.
+    //REQ:
+    //MOD:
+    void iniciar();
+    
+    //EFE: Destruye la Lista, la deja inutilizable.
+    //REQ: L inicializado.
+    //MOD: La Lista.
+    void destruir();
+    
+    //EFE: Elimina todos los elementos de la Lista.
+    //REQ: L inicializado.
+    //MOD: La Lista.
+    void vaciar();
+    
+    //EFE: Retorna true si esta vacia y false en caso contrario.
+    //REQ: La Lista inicializada.
+    //MOD:
+    bool vacia();
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    void agregar(V elemento, int indice);
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    void borrar(int indice);
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    V recuperar(int indice);
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    void modificarElemento(V elemento, int indice);
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    void intercambiar(int i, int j);
+    
+    //EFE:
+    //REQ:
+    //MOD:
+    int numElem();
+};
 
 #endif /* LISTAINDEN_H */
 
